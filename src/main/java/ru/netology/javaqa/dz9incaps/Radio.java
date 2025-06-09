a9766ce3f994584f6ede40ffff25093e23f48abe
neshutnick <neshutnick89@gmail.com>
2025-06-10 02:36:38 +0300

    Add custom number of radio stations

--- src/main/java/ru/netology/javaqa/dz9incaps/Radio.java
+++ src/main/java/ru/netology/javaqa/dz9incaps/Radio.java
@@ -1,8 +1,22 @@
 package ru.netology.javaqa.dz9incaps;
 
 public class Radio {
+    
+    private int maxStation;
     private int currentStation;
     private int currentVolume;
+    
+    public Radio() {
+        this.maxStation = 9; // По умолчанию 10 станций (0-9)
+    }
+
+    public Radio(int stationCount) {
+        if (stationCount <= 0) {
+            this.maxStation = 9; // Если передано некорректное значение, используем значение по умолчанию
+        } else {
+            this.maxStation = stationCount - 1; // Максимальный номер станции = количество станций - 1
+        }
+    }
 
     // Методы для работы с радиостанцией
     public int getCurrentStation() {
@@ -13,14 +27,14 @@ public class Radio {
         if (station < 0) {
             return;
         }
-        if (station > 9) {
+        if (station > maxStation) {
             return;
         }
         currentStation = station;
     }
 
     public void next() {
-        if (currentStation == 9) {
+        if (currentStation == maxStation) {
             currentStation = 0;
         } else {
             currentStation = currentStation + 1;
@@ -29,7 +43,7 @@ public class Radio {
 
     public void prev() {
         if (currentStation == 0) {
-            currentStation = 9;
+            currentStation = maxStation;
         } else {
             currentStation = currentStation - 1;
         }
@@ -50,5 +64,10 @@ public class Radio {
         if (currentVolume > 0) {
             currentVolume = currentVolume - 1;
         }
+    }
+
+    // Геттер для максимального номера станции (для тестов)
+    public int getMaxStation() {
+        return maxStation;
     }
 } 
